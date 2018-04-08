c2823042f64d5b169e6c950f0ff04e5b0d582b2b
Tylinese <tylinese@gmail.com>
2018-04-07 18:40:28 -0700

    deleted terraform so Tyler can add his own branch with his own commit

--- terraform.py
+++
@@ -1,8 +0,0 @@
-# Tyer Danh
-import re
-
-def get_total_terraformable_planets(content:str):
-	pattern = re.compile("\"TerraformState\":\"Terraformable\"")
-	result = pattern.findall(content)
-	count = len(result)
-	return count
